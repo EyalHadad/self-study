f2c61bd582f5568ebd788d0c1c51c7ce2e36053b
Eyal <eyalhadad1989@gmail.com>
2020-03-10 16:25:48 +0200

    Gan code

--- general_python/set_collections.py
+++ general_python/set_collections.py
@@ -1,13 +1,44 @@
-import mysql.connector
+import sqlite3
+import pandas as pd
+from pandasql import sqldf
+from pandasql import load_births
 
-mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="db_username",
-  passwd="db_password"
-)
+# print(sqlite3.version)
+# print(sqlite3.sqlite_version)
 
-# print(mydb)
+def memory_sql_database():
+    db = sqlite3.connect(':memory:')
+    # to connect an existing DB, in our case we connect to out memory and not remote DB
+    cursor = db.cursor()
+    # Use to iterate over the DB
+    cursor.execute('''CREATE TABLE books(id INTEGER PRIMARY KEY,title TEXT, author TEXT, price TEXT, year TEXT)''')
+    db.commit()
+    cursor.execute('''INSERT INTO books values(1,'pro powerShell','Bryan', 35.00, 2015)''')
+    cursor.execute('''INSERT INTO books values(2,'Hothiker','Adams', 12.00, 199)''')
+    db.commit()
+    lsbooks = cursor.execute('''select * from books;''').fetchall()
+    dfbook = pd.read_sql_query("SELECT * FROM books", db)
+    db.commit()
+    print(lsbooks)
+    print(dfbook.head())
+    cursor.close()
 
-mycursor = mydb.cursor()
+def chinook_sql_database():
+    conn = sqlite3.connect('chinook.db')
+    # to connect an existing DB, in our case we connect to out memory and not remote DB
+    cursor = conn.cursor()
+    # Use to iterate over the DB
+    albums = cursor.execute('''select * from albums order by title limit 3;''').fetchall()
+    # fetchall read all records into memory and return a list.
+    print(albums)
 
-mycursor.execute("CREATE DATABASE mydatabase")
+    dfalbum = pd.read_sql_query("SELECT * FROM albums", conn)
+    print(dfalbum.head())
+    dftablist = pd.read_sql_query("SELECT name FROM sqlite_master where type = 'table';", conn)
+    print(dftablist.head())
+    conn.close()
+    i=4
+
+if __name__ == '__main__':
+    chinook_sql_database()
+    # memory_sql_database()
